5457bc9f6b20fff7478bc0c93183fe1682bcaef0
Angelika Ymydykova <a.ymydykova@gmail.com>
2018-04-17 17:45:17 +0300

    Add variables, methods. Instantiate the objects

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,19 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    if (this.x < 505) {
+        this.x += this.speed * dt;
+    } else {
+        this.x = 0
+        this.x += this.speed * dt;
+    }
+
+    if ((player.x > this.x && player.x < this.x + 5) && 
+        (player.y > this.y && player.x < this.y + 5)) {
+        alert('Wasted');
+        return;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,11 +41,45 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(x, y) {
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+    this.speed = 100;
+}
+
+Player.prototype.update = function() {
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyCode) {
+    if (keyCode === 'left' && !(this.x - this.speed < 50)) {
+        this.x -= this.speed;
+    } else if (keyCode === 'up' && !(this.y - this.speed < 50)) {
+        this.y -= this.speed;
+    } else if (keyCode === 'right' && !(this.x + this.speed > 450)) {
+        this.x += this.speed;
+    } else if (keyCode === 'down' && !(this.y + this.speed > 500)) {
+        this.y += this.speed;
+    }
+    
+    if (this.y < 50) {
+        alert('You win');
+        return;
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var Enemy1 = new Enemy(0, 140, 100);
+var Enemy2 = new Enemy(0, 220, 120);
+var Enemy3 = new Enemy(0, 300, 70);
+var allEnemies = [Enemy1, Enemy2, Enemy3];
+var player = new Player(200, 400);
 
 
 // This listens for key presses and sends the keys to your
