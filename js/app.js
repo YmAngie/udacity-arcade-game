7338359368bb8032df1a3b6488e393569acb8a76
Angelica Ymydykova <aymydykova@oggettoweb.com>
2018-04-18 17:01:47 +0300

    Add ability to set the Enemies' number. Randomize the Enemies' speed. Refactor code.
    Delete comments and unnecessary Player.update() method

--- js/app.js
+++ js/app.js
@@ -1,23 +1,11 @@
-// Enemies our player must avoid
-var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
+    this.x = -120;
+    this.y = [60, 140, 220][Math.floor(Math.random() * 3)];
+    this.speed = Math.random() * (310 - 120) + 120;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
     this.x += this.speed * dt;
 
     if (this.x >= 505) {
@@ -32,24 +20,16 @@ Enemy.prototype.update = function(dt) {
     }
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 var Player = function(x, y) {
     this.sprite = 'images/char-boy.png';
-    this.x = x;
-    this.y = y;
+    this.reset();
     this.xStep = 505 / 5;
     this.yStep = (535 - 40) / 6
-};
-
-Player.prototype.update = function() {
 };
 
 Player.prototype.render = function() {
@@ -78,18 +58,24 @@ Player.prototype.reset = function() {
     this.y = 380;
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-var Enemy1 = new Enemy(-120, 140, 250);
-var Enemy2 = new Enemy(-120, 220, 130);
-var Enemy3 = new Enemy(-120, 60, 180);
-var allEnemies = [Enemy1, Enemy2, Enemy3];
-var player = new Player(200, 380);
+
+var player = new Player();
+var allEnemies = [];
+
+var userEnemyCount = prompt('How many Enemies will you prefer?', 'Insert a number from 1 to 7');
+var enemyCount;
+
+if (!isNaN(userEnemyCount) && userEnemyCount !== '') {
+    enemyCount = parseInt(userEnemyCount, 10);
+} else {
+    enemyCount = 3;
+}
+
+for (i = 1; i <= enemyCount; i++) {
+    allEnemies.push(new Enemy());
+}
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
